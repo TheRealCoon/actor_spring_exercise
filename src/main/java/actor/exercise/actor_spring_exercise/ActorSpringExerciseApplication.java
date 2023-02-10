7bba1429f33c2fe79754a71d0807c82db8c11174
T.Sanyi <s.truczkai@gmail.com>
2023-02-10 10:21:28 +0100

    starter build

---
+++ src/main/java/actor/exercise/actor_spring_exercise/ActorSpringExerciseApplication.java
@@ -0,0 +1,13 @@
+package actor.exercise.actor_spring_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorSpringExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActorSpringExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/actor/exercise/actor_spring_exercise/ActorSpringExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package actor.exercise.actor_spring_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorSpringExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
